8fd43add54eb7324446b662426636eb2db80d65c
sprintbiz <pawel.sawukajtys@gmail.com>
2018-08-11 17:01:25 +0200

    initial version

--- WDSCApp/MainPage.xaml.cs
+++ WDSCApp/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices.Client;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +27,124 @@ namespace WDSCApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Task msg;
+        private const int PIN1 = 18;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        const string deviceConnectionString = "HostName=home-IoT-hub1.azure-devices.net;DeviceId=Device1;SharedAccessKey=R0br8n7yZ2ii1ry3aD0Nsjczp38ebTS2JD38+upw3vs=";
+        static DeviceClient deviceClient = null;
+        string CurrentStatus = "";
+        Windows.UI.Core.CoreDispatcher dispatcher = Windows.UI.Core.CoreWindow.GetForCurrentThread().Dispatcher;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation eas =
+                new Windows.Security.ExchangeActiveSyncProvisioning.EasClientDeviceInformation();
+            DeviceBox.Text = eas.FriendlyName;
+
+            ReceiveCloudToDeviceMessageAsync();
+
+
+
         }
+
+
+        private static void CreateClient()
+        {
+            if (deviceClient == null)
+            {
+                // create Azure IoT Hub client from embedded connection string
+                deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+            }
+        }
+
+        private async Task<string> ReceiveCloudToDeviceMessageAsync()
+        {
+            CreateClient();
+            
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    await deviceClient.CompleteAsync(receivedMessage);
+
+                    Debug.WriteLine(messageData);
+                    if (messageData == "on")
+                    {
+                        receiveMessage(messageData);
+                        CurrentStatus = messageData;
+                        InitGPIO(PIN1);
+                    }
+
+                    if (messageData == "off")
+                    {
+                        receiveMessage(messageData);
+                        CurrentStatus = messageData;
+                    }
+
+                    if (messageData == "status")
+                    {
+                        receiveMessage(messageData);
+                        
+                    }
+                    //return messageData;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+        private async void receiveMessage(string message)
+        {
+            await dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                MessageBox.Text = "Received message: " + message;
+            });
+        }
+
+        private void InitGPIO(int GPIOPin)
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(GPIOPin);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pin initialized correctly.";
+
+        }
+
+        private void TurnOn(int GPIOPin)
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                pin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+
+            pin = gpio.OpenPin(GPIOPin);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pin initialized correctly.";
+
+        }
+
     }
 }
